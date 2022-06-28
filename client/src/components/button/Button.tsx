cb7edbb98bbe75a8fe0fade93593d0df6043d350
naamagi <n.gilat@gmail.com>
2022-06-28 23:21:10 +0300

    changed code to typescript

--- client/src/components/button/Button.tsx
+++ client/src/components/button/Button.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./buttonStyle.css";
 
-const Button = ({ btnText }) => {
+interface ButtonProps {
+   
+    btnText: string;
+}
+
+const Button = ({ btnText}:ButtonProps) => {
     return <div className="btn-container">{btnText}</div>;
 };
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -13,7 +13,7 @@ i18next.init({
     interpolation: { escapeValue: false }, // React already does escaping
 });
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")  as HTMLElement);
 root.render(
     // <React.StrictMode>
     <I18nextProvider i18n={i18next}>
