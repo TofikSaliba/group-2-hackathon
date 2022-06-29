e80ff2c771e9169e2fcd63afc211fb1581d99de1
Yanir <yanir401@gmail.com>
2022-06-30 00:05:29 +0300

    fix crashed

--- server/middleware/story/splitContent.js
+++ server/middleware/story/splitContent.js
@@ -1,37 +1,41 @@
 export const splitContent = async (req, res, next) => {
   let oldSend = res.send;
   res.send = function (data) {
-    // do something with the data
     res.send = oldSend; // set function back to avoid the 'double-send'
 
-    const dividedPages = [];
-    const splittedContent = data.storyContent.split("\n\n");
-    let remainder = "";
-    for (let i = 0; i < splittedContent.length; i++) {
-      remainder += `${splittedContent[i]}\n\n`;
-      if (1100 <= remainder.length && remainder.length <= 1300) {
-        dividedPages.push(remainder);
-        remainder = "";
-        continue;
-      } else if (remainder.length < 1100) {
-        continue;
-      } else {
-        let j = 1300;
-        while (
-          j < remainder.length &&
-          remainder[j] !== "." &&
-          remainder[j] !== "!" &&
-          remainder[j] !== "?"
-        ) {
-          j++;
+    try {
+      if (!data.storyContent) throw new Error();
+      const dividedPages = [];
+      const splittedContent = data.storyContent.split("\n\n");
+      let remainder = "";
+      for (let i = 0; i < splittedContent.length; i++) {
+        remainder += `${splittedContent[i]}\n\n`;
+        if (1100 <= remainder.length && remainder.length <= 1300) {
+          dividedPages.push(remainder);
+          remainder = "";
+          continue;
+        } else if (remainder.length < 1100) {
+          continue;
+        } else {
+          let j = 1300;
+          while (
+            j < remainder.length &&
+            remainder[j] !== "." &&
+            remainder[j] !== "!" &&
+            remainder[j] !== "?"
+          ) {
+            j++;
+          }
+          const sliced = remainder.slice(0, j);
+          dividedPages.push(sliced);
+          remainder = remainder.slice(j);
         }
-        const sliced = remainder.slice(0, j);
-        dividedPages.push(sliced);
-        remainder = remainder.slice(j);
       }
-    }
 
-    return res.send(dividedPages);
+      return res.send(dividedPages);
+    } catch (error) {
+      res.send("not found");
+    }
   };
   next();
 };
