ada0b170d738f185e29c25c1c82e38ab4283dce6
shirtol <shirzahavi7@gmail.com>
2022-06-30 11:47:35 +0300

    Added spinner

--- client/src/components/spinner/Spinner.tsx
+++ client/src/components/spinner/Spinner.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./spinner.css";
 
 const Spinner = () => {
-    return <div>Spinner</div>;
+    return (
+        <div className="lds-roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
 };
 
 export default Spinner;
